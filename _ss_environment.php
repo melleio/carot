a827f2b273bbbd65dda86d32540534c070b3cf41
Lloyd Emelle <lloyd@emelle.me>
2014-05-07 19:06:15 -0400

    'updates'

--- _ss_environment.php
+++ _ss_environment.php
@@ -3,10 +3,15 @@
 //$_SERVER['HTTP_HOST'] = $allowed_hosts;
 //$_SERVER['REQUEST_METHOD'] = 'Get';
 define('SS_ENVIRONMENT_TYPE', 'dev');
-$host = (getenv('OPENSHIFT_MYSQL_DB_HOST')) ? getenv('OPENSHIFT_MYSQL_DB_HOST') : putenv('OPENSHIFT_MYSQL_DB_HOST=mycarot.c0pz9rduf4ic.us-east-1.rds.amazonaws.com') ;
+/*$host = (getenv('OPENSHIFT_MYSQL_DB_HOST')) ? getenv('OPENSHIFT_MYSQL_DB_HOST') : putenv('OPENSHIFT_MYSQL_DB_HOST=mycarot.c0pz9rduf4ic.us-east-1.rds.amazonaws.com') ;
 $port = (getenv('OPENSHIFT_MYSQL_DB_PORT')) ? getenv('OPENSHIFT_MYSQL_DB_PORT') : putenv('OPENSHIFT_MYSQL_DB_PORT=3306');
 $user = (getenv('OPENSHIFT_MYSQL_DB_USERNAME')) ? getenv('OPENSHIFT_MYSQL_DB_USERNAME') : putenv('OPENSHIFT_MYSQL_DB_USERNAME=carotUser') ;
 $pass = (getenv('OPENSHIFT_MYSQL_DB_PASSWORD')) ? getenv('OPENSHIFT_MYSQL_DB_PASSWORD') : putenv('OPENSHIFT_MYSQL_DB_PASSWORD=Bean1Jean') ;
+*/
+$host = putenv('OPENSHIFT_MYSQL_DB_HOST=mycarot.c0pz9rduf4ic.us-east-1.rds.amazonaws.com') ;
+$port = putenv('OPENSHIFT_MYSQL_DB_PORT=3306');
+$user = putenv('OPENSHIFT_MYSQL_DB_USERNAME=carotUser') ;
+$pass = putenv('OPENSHIFT_MYSQL_DB_PASSWORD=Bean1Jean') ;
 $repo_dir = (getenv('OPENSHIFT_REPO_DIR')) ? getenv('OPENSHIFT_REPO_DIR') : putenv('OPENSHIFT_REPO_DIR=/var/www/html/www/php') ;
 
 
