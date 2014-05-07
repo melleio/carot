40eb5f458c782e8f0d61762297d90d5aad984e11
Lloyd Emelle <lloyd@emelle.me>
2014-05-07 17:26:07 -0400

    'updates'

--- php/mysite/_config.php
+++ php/mysite/_config.php
@@ -4,22 +4,14 @@ global $project;
 $project = 'mysite';
 
 global $databaseConfig;
-/*$databaseConfig = array(
+$databaseConfig = array(
 	"type" => 'MySQLDatabase',
 	"server" => 'mycarot.c0pz9rduf4ic.us-east-1.rds.amazonaws.com',
 	"username" => 'carotUser',
 	"password" => 'Bean1Jean',
 	"database" => 'carot',
 	"path" => '',
-);*/
-
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => '127.1.245.2',
-	"username" => 'adminDqaggHF',
-	"password" => 'I-25-whqbqHl',
-	"database" => 'www',
-	"path" => '',
 );
+
 // Set the site locale
 i18n::set_locale('en_US');
