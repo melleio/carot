0bee0c5e883e7ecc2e6826ae2c1d7fc4f0dc3cee
Lloyd Emelle <lloyd@emelle.me>
2014-05-08 12:47:46 -0400

    updates

---
+++ php/mysite/code/LocationPage.php
@@ -0,0 +1,66 @@
+<?php
+class LocationPage extends Page {
+
+	private static $db = array(
+	);
+
+	/*function canCreate($Member = null){
+	    if(!Permission::check("EDIT_SITE")) Security::permissionFailure();
+	}*/
+
+	private static $has_many = array(
+    );
+
+    public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		return $fields;
+    }
+
+}
+class LocationPage_Controller extends Page_Controller {
+
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+	private static $allowed_actions = array (
+		'LatestTweet','TopImage'
+	);
+
+	public function init() {
+		parent::init();
+
+		// Security Check. Comment to disable
+		// if(!Permission::check("VIEW_SITE")) Security::permissionFailure();
+		//Requirements::set_write_js_to_body(false);
+		/*Requirements::combine_files(
+	    'home.ppd.js',
+	    array(
+	        'themes/v3/js/custom.js',
+	    	'themes/v3/js/home.js',
+	        'themes/v3/js/bootstrap.min.js',
+	        'themes/v3/js/chosen.jquery.min.js',
+	        'themes/v3/js/searchoverlay.js'
+	    )
+		);*/
+	}
+
+
+	/*public function HomeHeaderImage(){
+		return SiteConfig::get()->First()->HomeHeaderImages()->sort('RAND()')->First();
+	}*/
+
+
+
+}
