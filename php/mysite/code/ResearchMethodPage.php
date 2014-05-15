1ab14a7203ce9e8acec4681ab642e2ae5777e9c4
Lloyd Emelle <lloyd@emelle.me>
2014-05-15 09:53:20 -0400

    updates

---
+++ php/mysite/code/ResearchMethodPage.php
@@ -0,0 +1,77 @@
+<?php
+class ClinicalTrials extends ResearchPage {
+
+	private static $db = array(
+		'TopContent' => 'Text'
+	);
+
+	/*function canCreate($Member = null){
+	    if(!Permission::check("EDIT_SITE")) Security::permissionFailure();
+	}*/
+
+	private static $has_many = array(
+    );
+
+    public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$topcontent = new TextAreaField('TopContent');
+		$fields->addFieldToTab('Root.Main', $topcontent,'MenuTitle');
+		
+		$config2 = new GridFieldConfig_RelationEditor();
+		$config2->addComponents(new GridFieldExportButton('before'));
+		$config2->addComponent(new GridFieldSortableRows('SortOrder'));
+		$toolbox = GridField::create('GalleryImages',false, $this->GalleryImages()->sort('SortOrder'), $config2);
+		if(Permission::check("ADMIN")){
+			$fields->addFieldToTab('Root.GalleryImages', $toolbox);
+		}
+		return $fields;
+    }
+
+}
+class ClinicalTrials_Controller extends ResearchPage_Controller {
+
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+	private static $allowed_actions = array (
+		'LatestTweet','TopImage'
+	);
+
+	public function init() {
+		parent::init();
+
+		// Security Check. Comment to disable
+		// if(!Permission::check("VIEW_SITE")) Security::permissionFailure();
+		//Requirements::set_write_js_to_body(false);
+		/*Requirements::combine_files(
+	    'home.ppd.js',
+	    array(
+	        'themes/v3/js/custom.js',
+	    	'themes/v3/js/home.js',
+	        'themes/v3/js/bootstrap.min.js',
+	        'themes/v3/js/chosen.jquery.min.js',
+	        'themes/v3/js/searchoverlay.js'
+	    )
+		);*/
+	}
+
+
+	/*public function GalleryImages(){
+		return SiteConfig::get()->First()->HomeHeaderImages()->sort('RAND()')->First();
+	}*/
+
+
+
+}
---
+++ php/themes/c/templates/Layout/ResearchMethodPage.ss
@@ -0,0 +1,30 @@
+<div class="slide story" id="slide-1" data-slide="1" ng-controller="MainCtrl">
+<div class="container">
+
+	<% include BreadCrumbs %>
+	
+	<div class="row">
+		<div class="col-sm-12">
+			<% include ResearchGallery %>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12" style="background:url('http://www-carot.us.melle.io/assets/Research/Clinical-Trials/Clinical-Trials-Main/other-graphic-elements/Clinical-Trials-Image-Bed-for-Mission-Statement-.png') no-repeat;background-size:cover;background-position: 100% 100%;padding:2.5%;margin:1%;">
+			<p><% if TopContent %> $TopContent <% end_if %></p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-12">
+			<h1 style="color:red;text-transform:uppercase;">$Title</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12 gray-box" style="background:url('http://www-carot.us.melle.io/assets/Research/Clinical-Trials/CHM/CHM-Image-Bed-for-Text-on-Left-.png');background-size:cover;background-position: 100% 100%;">
+			<div class="content-text">
+			$Content
+			</div>
+		</div>
+	</div>
+
+</div>
