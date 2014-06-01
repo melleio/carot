4e26c85fb7770ad7a512f22c762fa7c0c2c1bbb3
Lloyd Emelle <lloyd@emelle.me>
2014-06-01 01:38:04 -0400

    updates

--- php/mysite/code/GalleryService.php
+++ php/mysite/code/GalleryService.php
@@ -34,7 +34,7 @@ class GalleryService implements WebServiceable {
             # code...
             $t = array('src'=>$image->Image()->AbsoluteURL,
                 'title'=> $image->Image()->Title,
-                'description'=> $image->Description);
+                'description'=> $image->Description->Raw());
             array_push($a,$t);
         }
         return array(
