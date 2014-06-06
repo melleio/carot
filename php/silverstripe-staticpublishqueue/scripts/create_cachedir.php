bf821222a4195234f00082938a59a9ea7ffd1324
Lloyd <l@emelle.me>
2014-06-05 23:29:16 -0400

    updates

--- php/silverstripe-staticpublishqueue/scripts/create_cachedir.php
+++ php/silverstripe-staticpublishqueue/scripts/create_cachedir.php
@@ -1,6 +1,6 @@
 <?php
-if (!file_exists('web_docs')) {
-	mkdir('web_docs'); //create a new cache dir
+if (!file_exists('cache')) {
+	mkdir('cache'); //create a new cache dir
 }
-chmod('web_docs',0777);    //make sure it is readable by the web-server user
+chmod('cache',0777);    //make sure it is readable by the web-server user
 echo "Success: created 'cache' folder that contains the statically published files\n";
