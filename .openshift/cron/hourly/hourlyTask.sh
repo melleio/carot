d4b2b4c86bbb5eae7a5c92fbde86eac5f51ebf86
Lloyd <l@emelle.me>
2014-06-06 15:40:10 -0400

    updates

---
+++ .openshift/cron/hourly/hourlyTask.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#export COMPOSER_HOME="$OPENSHIFT_DATA_DIR/.composer"
+
+# if [ ! -f "$OPENSHIFT_DATA_DIR/composer.phar" ]; then
+#     curl -s https://getcomposer.org/installer | php -- --install-dir=$OPENSHIFT_DATA_DIR
+# else
+#     	php $OPENSHIFT_DATA_DIR\composer.phar self-update
+# fi
+cd /home/carot/repo\php
+# git clone https://github.com/nyeholt/silverstripe-webservices
+
+# php framework/cli-script.php dev/build flush=1
+php framework/cli-script.php rabbit
+
+php $OPENSHIFT_REPO_DIR/php/framework/cli-script.php dev/tasks/SiteTreeFullBuildEngine urls=/
+php $OPENSHIFT_REPO_DIR/php/framework/cli-script.php dev/tasks/BuildStaticCacheFromQueue
